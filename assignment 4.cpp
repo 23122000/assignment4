0ab008b05c8dd4eacc5074fce644dce1b6a17dfb
23122000 <109691496+23122000@users.noreply.github.com>
2022-07-20 23:42:06 +0530

    Add files via upload

---
+++ assignment 4.cpp
@@ -0,0 +1,82 @@
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=5)
+    {
+
+
+        printf("\nMySirG ");
+    i++;
+}
+printf("\n");
+int x=1;
+
+ while(x<=10)
+ {
+     printf("%d ",x);
+     x++;
+
+ }
+printf("\n");
+ int p=1;
+ while(p<=19)
+ {
+     printf("%d ",p);
+     p=p+2;
+ }
+ printf("\n");
+
+int r=2;
+while(r<=20)
+{
+    printf("%d  ",r);
+    r=r+ 2;
+}
+printf("\n");
+int t;
+int s=1;
+while(s<=10)
+{ t=s*s;
+    printf("%d ",t);
+    s++;
+
+}
+printf("\n");
+int d, e=1;
+while(e<=10)
+{ d=e*e*e;
+    printf("%d ",d);
+    e++;
+}
+printf("\n");
+int j= 19;
+
+
+ for(j=19;j>= 1;j=j-2)
+ {
+
+
+     printf("%d  ",j);
+
+
+ }
+ printf("\n");
+ int k= 20;
+ for(k=20;k>=2;k= k-2)
+ {
+     printf("%d ",k);
+ }
+ printf("\n");
+ int f= 10;
+ for(f=10;f>=1;f--)
+ {
+     printf("%d ",f);
+ }
+printf("\n");
+int l=1;
+for(l=1; l<= 10;l++)
+{
+    printf(" \n 5  x  %d = %d " ,l, 5*l);
+}
+}
